3344cbcb0538f5b5e367b68a77679b3bde9fff5c
Salem Kallien <31890883+SalemKitkat@users.noreply.github.com>
2018-11-25 20:53:56 +0000

    Add stuff to core instead of ignoring it (no way near complete lmao)

--- sys-low/core/core.cmd
+++ sys-low/core/core.cmd
@@ -1,2 +1,16 @@
 @Echo Off
-:: Stuff will be added later
+:Core
+:: Globals
+Set "Core_Version=0.0.1"
+Set "Core_Directory=%cd%"
+Set "Core_Arch=CS"
+
+:: Architecture Variables (Only edit if making a custom core)
+Set "Core_ArchVersion=0.0.1"
+Set "Core_ArchName=Cyan Silver"
+
+:: Features (0 = False, 1 = True)
+:: Allow system files to be directly loaded?
+Set "Core_AllowModuleOffSet=0"
+:: Require user login on boot?
+Set "Core_RequireUser=1"
