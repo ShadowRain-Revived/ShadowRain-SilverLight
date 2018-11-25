212225a807230b024671a8987e574e2efb3521e5
Salem Kallien <31890883+SalemKitkat@users.noreply.github.com>
2018-11-25 20:59:40 +0000

    Update sys-index.cmd

--- sys-low/sys-index.cmd
+++ sys-low/sys-index.cmd
@@ -1,2 +1,10 @@
 @Echo Off
-:: Stuff will be added here later
+If "%1"=="-LoadCore" Goto LoadCore
+:: If "%1"=="-LoadBack" Goto LoadBack
+Exit
+
+:LoadCore
+core\core.cmd
+
+:: :LoadBack
+:: will add stuff here later
