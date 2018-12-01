c387f1b08b1c36786cae12c2af58bed779f98d32
Zach <18561222+zellman01@users.noreply.github.com>
2018-12-01 14:13:24 -0500

    Add files via upload

---
+++ modules/0.0.1.cmd
@@ -0,0 +1,57 @@
+@Echo Off
+SetLocal EnableExtensions EnableDelayedExpansion
+if "%1"=="" Goto Entry
+
+:Loop
+Set currentDir=%cd%
+::Cd !shadowDir!
+EndLocal
+::modules\0.0.4.bat -LoadModule
+Set Command=
+
+:Entry
+Set /P Command="<%cd%> (~test) "
+For %%a In (!Command!) Do (
+    Set /A count+=1
+    Set Value!count!=%%a
+)
+If "!Value1!"=="" Goto Loop
+If "!Value1!"=="""" Goto Loop
+If "!Value1!"=="md" Goto MkDir
+If "!Value1!"=="rd" Goto RD
+
+:MkDir
+If "!Value2!"=="" (
+	Echo.
+	Echo Please specify what directory to make.
+	Set Command=
+	Goto Loop
+)
+If Exist "!Value2!" (
+	Echo.
+	Echo This directory already exists.
+	Set Command=
+	Goto Loop
+)
+Mkdir "!Value2!"
+Echo '!Value2!' has been made.
+Set Command=
+Goto Loop
+
+:RD
+If Not Exist "!Value2!" (
+	Echo.
+	Echo This directory does not exist
+	Set Command=
+	Goto Loop
+)
+Set /P Confirm="Are you sure you want to delete !Value2!? (yes/no): "
+If "!Confirm!"=="yes" (
+	Rd /S /Q !Value2!
+	Echo '!Value2!' has been deleted.
+)
+If Not "!Confirm!"=="yes" If Not "!Confirm!"=="no" (
+	Echo Please answer yes or no only.
+)
+Set Command=
+Goto Loop
