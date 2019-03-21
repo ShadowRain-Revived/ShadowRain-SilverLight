2dda781eceb7046e9c7eb1830c0f5921f2f258e6
Salem Kallien <31890883+SalemKitkat@users.noreply.github.com>
2019-03-21 15:52:15 +0000

    sys-index isn't an index file, its an initialisation process

---
+++ sys/sys_init.cmd
@@ -0,0 +1,20 @@
+@Echo Off
+:: Load the core.
+If "%1"=="-LoadCore" If "%2"=="PreInit" If "%3"=="System.System" Goto System.loadCore
+:: Core is loaded, Load Graphics (Not ready).
+:: If "%1"=="-CorePreLoaded" If "%2"=="Core.Core" If "%3"=="True" Goto System.loadGraphics
+If "%Core.AllowModuleOffSet%"=="False" (
+	Echo Unable to run file directly, It is disallowed by the core.
+	Exit
+)
+	
+:System.loadCore
+core\core -PreInit Core.Core
+Echo Something happened.
+Exit
+
+:: Not ready yet
+:: :System.loadGraphics
+:: graphics\graphics -PreInit Graphics.Graphics
+:: Echo Something happened.
+:: Exit
