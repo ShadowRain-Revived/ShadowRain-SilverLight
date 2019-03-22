b0171944d2974aa2385f12d7eaa7b2fe22330c3e
Salem Kallien <31890883+SalemKitkat@users.noreply.github.com>
2019-03-22 12:17:14 +0000

    (Graphics) Add a value check just in case values were not set

--- sys/graphics/graphics.cmd
+++ sys/graphics/graphics.cmd
@@ -15,12 +15,16 @@ If "%Core.EnableGraphics%"=="True" (
 	
 	:: ShadowRain's Default color is Purple (0D)
 	If Not Defined Graphics.DefaultColor Set "Graphics.DefaultColor=0D"
+	
 	:: Color when processing Information (0B)
 	If Not Defined Graphics.Information Set "Graphics.Information=0B"
+	
 	:: Color when processing an install script (8F)
 	If Not Defined Graphics.ScriptColor Set "Graphics.ScriptColor=8F"
+	
 	:: Color when processing an error (04)
 	If Not Defined Graphics.ErrorColor Set "Graphics.ErrorColor=04"
+	
 	:: Color when processing a major system fault (5F)
 	If Not Defined Graphics.SystemFault Set "Graphics.SystemFault=5F"
 ) Else (
@@ -30,4 +34,27 @@ If "%Core.EnableGraphics%"=="True" (
 	If Not Defined Graphics.ScriptColor Set "Graphics.ScriptColor=0F"
 	If Not Defined Graphics.ErrorColor Set "Graphics.ErrorColor=0F"
 	If Not Defined Graphics.SystemFault Set "Graphics.SystemFault=0F"
+)
+
+:: Check if all required values are set.
+If Defined Graphics.DefaultColor (
+	If Defined Graphics.Information (
+		If Defined Graphics.ScriptColor (
+			If Defined Graphics.ErrorColor (
+				If Defined Graphics.SystemFault (
+					:: do something here
+				) Else (
+					:: value "Graphics.SystemFault" isn't set
+				)
+			) Else (
+				:: value "Graphics.ErrorColor" isn't set
+			)
+		) Else (
+			:: value "Graphics.ScriptColor" isn't set
+		)
+	) Else (
+		:: value "Graphics.Information" isn't set
+	)
+) Else (
+	:: value "Graphics.DefaultColor" isn't set
 )
